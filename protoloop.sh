99d653a3942c8f509411e92a507b60aba79aeae8
Anton Alekseevich <AntonBoch1244@users.noreply.github.com>
2019-03-21 00:52:21 +0400

    Update protoloop.sh
    Change list:
    + Restored 90% of old content
    * Behavior: Uses file-checking instead PID variable

--- protoloop.sh
+++ protoloop.sh
@@ -1,5 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -x
 
-make
+function notcup() {
+	
+	if [ -f ./.pid ]
+	then if [ -z `cat ./.pid` ]
+		then
+			rm ./.pid
+		else
+			kill -9 `cat ./.pid`
+			rm ./.pid			
+		fi
+	fi
+
+	make clean
+
+}
+
+trap notcup 2
+
+while [ true ]
+do
+	make -k all
+	if [ "$?" -eq "0" ]
+	then
+		./proto &
+		echo $! > ./.pid
+		inotifywait -e modify -e delete -e move proto.c
+		notcup
+	else
+		exit 0
+	fi
+done
