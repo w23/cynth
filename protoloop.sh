0414e2bb8c19a400b6e1d496234f0e08b01ca48a
Ivan Avdeev <me@w23.ru>
2019-03-24 15:22:11 +0700

    Make monitoring script usable with other modules

--- protoloop.sh
+++ protoloop.sh
@@ -2,30 +2,38 @@
 
 set -x
 
+CYNTH=$1
+if [[ -z "${CYNTH}" ]]
+then
+	echo "Usage: $0 source-to-monitor.c"
+	exit 1
+fi
+
 PID=
 
 function trap_ctrlc ()
 {
+	echo "Exit requested"
 	[[ -z "${PID}" ]] || kill $PID
 	exit 2
 }
 
 trap "trap_ctrlc" 2
 
-CFLAGS=-Iatto/include
-SOURCES="proto.c protovis.c midi_alsa.c atto/src/app_linux.c atto/src/app_x11.c"
+CFLAGS="-Wall -Werror -Iatto/include"
+SOURCES="protovis.c midi_alsa.c atto/src/app_linux.c atto/src/app_x11.c"
 
 while [ true ]
 do
-	cc $CFLAGS -lGL -lX11 -lXfixes -lasound -pthread -lm $SOURCES -o proto
+	cc $CFLAGS -lGL -lX11 -lXfixes -lasound -pthread -lm "$CYNTH" $SOURCES -o "$CYNTH.exe"
 	if [ $? -eq 0 ]
 	then
-		./proto &
+		./"$CYNTH".exe &
 		PID=$!
-		inotifywait -e modify -e delete -e move proto.c
+		inotifywait -e modify -e delete -e move "$CYNTH"
 		kill $PID
 		PID=
 	else
-		sleep 1
+		inotifywait -e modify -e delete -e move "$CYNTH"
 	fi
 done
